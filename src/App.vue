8e1a9b055c497a1ccdfecdea6ef707122cbad347
cg <654450188@qq.com>
2017-07-12 16:48:46 +0800

    修改README Please enter the commit message for your changes. Lines starting

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,9 @@
 
 <script>
 import header from 'components/header/header.vue'
-import {urlParse} from 'common/js/util.js'
+import {
+  urlParse
+} from 'common/js/util.js'
 
 const ERR_OK = 0
 
@@ -36,7 +38,7 @@ export default {
   data() {
     return {
       seller: {
-        id: (() => {  // 每个商家都有一个id标识
+        id: (() => { // 每个商家都有一个id标识
           let queryParam = urlParse()
           // console.log(queryParam)
           return queryParam.id
@@ -47,10 +49,10 @@ export default {
   created() {
     this.$http.get(`/api/seller?id=${this.seller.id}`).then((res) => {
       if (res.data.errno === ERR_OK) {
-          // this.seller = res.data.data // 此方法id会被覆盖丢弃，改用Object.assign方法
+        // this.seller = res.data.data // 此方法id会被覆盖丢弃，改用Object.assign方法
         this.seller = Object.assign({}, this.seller, res.data.data)
-          /* Object.assign 是es6语法，类似jQuery的$extend方法，详见MDN文档
-          https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/assign */
+        /* Object.assign 是es6语法，类似jQuery的$extend方法，详见MDN文档
+        https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/assign */
         // console.log('seller', this.seller)
         // console.log('seller.id', this.seller.id)
       }
