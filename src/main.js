992f7ca33431b7cada4b5dffc409e2f58b902c60
chengong <654450188@qq.com>
2017-10-05 15:09:54 +0800

    api110

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import axios from 'axios'
-import App from './App'
-import router from './router/index.js'
-import 'common/sass/index.scss'
+import Vue from 'vue';
+import axios from 'axios';
+import App from './App';
+import router from './router/index.js';
+import 'common/sass/index.scss';
 
 Vue.config.productionTip = false
 
